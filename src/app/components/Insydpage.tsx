4b40fcdbc377a9a2cffad8130b6c390a95809371
Ishan Gupta <ishangupta@Ishans-MacBook-Pro.local>
2025-09-26 12:45:10 +0530

    fixed insyd images

--- src/app/components/Insydpage.tsx
+++ src/app/components/Insydpage.tsx
@@ -8,10 +8,28 @@ export default function Insydpage() {
       <div className="absolute inset-0 bg-pink-800 blur-3xl h-60 z-0" />
 
       {/* Images placed ABOVE the pink bar */}
-      <div className="absolute inset-0 flex justify-center items-center gap-30 z-10">
-        <Image src="/insyd1.png" alt="insyd1" width={220} height={413} />
-        <Image src="/insyd2.png" alt="insyd2" width={220} height={413} />
-        <Image src="/insyd3.png" alt="insyd3" width={220} height={413} />
+      <div className="absolute inset-0 flex justify-center items-center gap-8 z-10">
+        <Image 
+          src="/insyd1.png" 
+          alt="insyd1" 
+          width={220} 
+          height={413}
+          className="rounded-lg"
+        />
+        <Image 
+          src="/insyd2.png" 
+          alt="insyd2" 
+          width={220} 
+          height={413}
+          className="rounded-lg"
+        />
+        <Image 
+          src="/insyd3.png" 
+          alt="insyd3" 
+          width={220} 
+          height={413}
+          className="rounded-lg"
+        />
       </div>
     </div>
   );
